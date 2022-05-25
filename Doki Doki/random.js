b40068d7259694293cdfbbb3e2363c872ca212cb
Rafael Carvalho <rafarete802@gmail.com>
2022-05-25 17:31:31 -0300

    Doki Doki Choice

---
+++ Doki Doki/random.js
@@ -0,0 +1,16 @@
+function monika(){
+  var result = document.getElementById('monika')
+  result.src = 'https://th.bing.com/th/id/R.71b544616eb8a0d95ced003c3cae844c?rik=GmG%2fMM2ipibCDA&pid=ImgRaw&r=0'
+}
+function yuri(){
+  var result = document.getElementById('yuri')
+  result.src = 'https://th.bing.com/th/id/R.71b544616eb8a0d95ced003c3cae844c?rik=GmG%2fMM2ipibCDA&pid=ImgRaw&r=0'
+}
+function sayori(){
+  var result = document.getElementById('sayori')
+  result.src = 'https://th.bing.com/th/id/R.71b544616eb8a0d95ced003c3cae844c?rik=GmG%2fMM2ipibCDA&pid=ImgRaw&r=0'
+}
+function natsuki(){
+  var result = document.getElementById('natsuki')
+  result.src = 'https://tse3.mm.bing.net/th?id=OIP.22Id9bA5UvjST6oDlflIaAAAAA&pid=Api&P=0&w=138&h=159'
+}
